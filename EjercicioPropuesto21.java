e2bc7ece7c82ed2bec4eee7ad1fe625d82b467a6
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:03:19 -0500

    Ejercicio finalizado

--- EjercicioPropuesto21.java
+++ EjercicioPropuesto21.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class EjercicioPropuesto21 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        System.out.println("Ingrese el primer lado del triangulo: ");
+        System.out.println("Ingrese el primer lado del triangulo: "); //Ingresamos datos por teclado
         int lado1=entrada.nextInt();
         System.out.println("Ingrese el segundo lado del triangulo: ");
         int lado2=entrada.nextInt();
         System.out.println("Ingrese el tercer lado del triangulo: ");
         int lado3=entrada.nextInt();
-        int perimetro=lado1+lado2+lado3;
+        int perimetro=lado1+lado2+lado3; //Hacemos los respectivos calculos
         double semiperimetro=(lado1+lado2+lado3)/2;
         //Formula de Heron:
         double area=Math.sqrt((semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-lado3)));
